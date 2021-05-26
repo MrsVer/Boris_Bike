500c4483391f4eedf184f0e03b80dd9a84241045
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-05-26 19:50:00 +0100

    Challange 11 solved

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,19 @@
-require 'docking_station.rb'
+require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :release_bike }
 
-    it 'releases working bikes' do
-        bike = subject.release_bike
-        expect(bike).to be_working
-      end
-end
+  it 'releases working bikes' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    attr_reader :bike
+end 
